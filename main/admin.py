8a46180407a28235a257341bae30a79f62c80ed3
gtless <330887712@qq.com>
2025-07-22 22:00:15 +0800

    修改admin管理

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from main.models import DishOrderTable, UserInputDishTable
+
+admin.site.register(DishOrderTable)
+admin.site.register(UserInputDishTable)
 # Register your models here.
